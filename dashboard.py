d7f8fc4a6ae661c5ba67e6e8ec1b489ebcf00a19
Hagelstein, Kai <kai.hagelstein@spiegel.de>
2025-12-07 12:53:59 +0100

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,18 @@
+import os
+from time import sleep
+from inky.auto import auto
+
+# Create inky impression display handle
+inky_display = auto()
+
+while True:
+    # Take website screenshot
+    os.system("chromium --headless --screenshot=current.png --window-size=1600,1200 --disable-gpu --full-page --no-sandbox --timeout=3000 --hide-scrollbars http://nas:2356/dashboard/")
+
+    # Display image on inky impression
+    img = Image.open("current.png")
+    inky_display.set_image(img)
+    inky_display.show()
+
+    # Wait until repeat
+    sleep(3*60) # 3 minutes
