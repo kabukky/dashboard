4606efee3522ddfffbbc37b2591b47b2cd1b5c0e
Hagelstein, Kai <kai.hagelstein@spiegel.de>
2026-01-06 11:16:58 +0100

    Update dashboard_headless.py

--- dashboard_headless.py
+++ dashboard_headless.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service
@@ -10,5 +11,6 @@ options.add_argument("-height=1600")
 driver = webdriver.Firefox(service=service, options=options)
 
 driver.get("http://nas:2356/dashboard/v2/calendar/")
+time.sleep(3)
 driver.save_screenshot("screenshot.png")
 driver.quit()
