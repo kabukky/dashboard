e5c2d98e03495ad9c4568537b73e5f8a1c78a47b
Hagelstein, Kai <kai.hagelstein@spiegel.de>
2026-01-06 11:21:05 +0100

    Update dashboard_headless.py

--- dashboard_headless.py
+++ dashboard_headless.py
@@ -6,10 +6,11 @@ from selenium.webdriver.firefox.service import Service
 service = Service(executable_path="/usr/bin/geckodriver")
 options = FirefoxOptions()
 options.add_argument("--headless")
-options.add_argument("-width=1200")
-options.add_argument("-height=1600")
+# options.add_argument("-width=1200")
+# options.add_argument("-height=1600")
 driver = webdriver.Firefox(service=service, options=options)
 
+driver.set_window_size(1200, 1600)
 driver.get("http://nas:2356/dashboard/v2/calendar/")
 time.sleep(3)
 driver.save_screenshot("screenshot.png")
